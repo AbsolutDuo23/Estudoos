b9a0ab168172ae22f8d81fcd4a6895867440a84c
jonathan <felipe.jonathanjf@gmail.com>
2024-06-13 16:02:37 -0300

    variavel java

---
+++ estudoos/java/treino.java
@@ -0,0 +1,15 @@
+public class Treino {
+    public static void main(String[] args) {
+
+       // String nome; atribuir variavel tipo
+       // nome = "natalia";
+       int a; 
+       int b = 3;
+       a = 2;
+       int soma = a + b; 
+       int subtracao = a - b;
+       int mutlipicacao = a * b;
+       float divisao = (float)a / b;
+        System.out.println(divisao);
+    }
+}
